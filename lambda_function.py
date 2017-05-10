ef5be02697d8999bcfb400706183748e3a91ff40
uehara <b1015062@fun.ac.jp>
2017-05-10 09:55:42 +0900

    test

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,24 @@ import sys
 
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'vendor'))  # noqa
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# use creds to create a client to interact with the Google Drive API
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+# Find a workbook by name and open the first sheet
+# Make sure you use the right name here.
+sheet = client.open("lambda-bot-log").sheet1
+
 
 def lambda_handler(event, context):
+    list_of_hashes = sheet.get_all_records()
+    print(list_of_hashes)
+
     print('hello world')
 
 
