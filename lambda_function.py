ff0583374ab89e5a4042bd50b03c9e90509f931b
uehara <b1015062@fun.ac.jp>
2017-05-10 10:58:03 +0900

    update

--- lambda_function.py
+++ lambda_function.py
@@ -7,21 +7,21 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-# use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+def get_worksheet():
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("lambda-bot-log").sheet1
+    sheet = client.open("lambda-bot-log")
+    for worksheet in sheet.worksheets():
+        if worksheet.id == 'od6':
+            return worksheet
 
 
 def lambda_handler(event, context):
-    list_of_hashes = sheet.get_all_records()
-    print(list_of_hashes)
-
-    print('hello world')
+    worksheet = get_worksheet()
+    print(worksheet.get_all_values())
+    # worksheet.append_row(['tweet.id_str', 'tweet.author.screen_name', 'tweet.text', 'tweet.created_at'])
 
 
 if __name__ == '__main__':
--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 pip install -U -r requirements.txt -t ./vendor
-zip -r lambda-package.zip *.py vendor
+zip -r lambda-package.zip *.py vendor client_secret.json
